8d4654584f2148f725bb57e0c9a2df1ba6ba578a
aless10 <alessio.izzo86@gmail.com>
2020-04-08 12:02:11 +0200

    [fix] fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 
-from setuptools import setup, find_packages
+from setuptools import setup
 from pkg_resources import parse_requirements
 
 req_file = os.path.join(os.path.dirname(__file__), "requirements.txt")
@@ -9,17 +9,15 @@ req_file = os.path.join(os.path.dirname(__file__), "requirements.txt")
 with open(req_file, "r") as inst_reqs:
     install_requires = [str(req) for req in parse_requirements(inst_reqs)]
 
-packages = find_packages(include=["checkouter", "checkouter.*"])
-
 setup(
     name="LongS Checkouter",
     version="0.0.1",
     author="Me",
     description="",
-    packages=packages,
+    packages=[],
     entry_points='''
-        [console_scripts]
-        esselunga=checkouter.main.:main
+    [console_scripts]
+        esselunga=main:main
     ''',
     include_package_data=True,
     classifiers=[
