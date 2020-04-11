d7adf231bc07dbd442d04de0667cda1070f199e6
aless10 <alessio.izzo86@gmail.com>
2020-04-11 15:21:50 +0200

    Bump version: 0.0.1 → 0.1.0.dev

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(req_file, "r") as inst_reqs:
 
 setup(
     name="LongS Checkouter",
-    version="0.0.1",
+    version="0.1.0.dev",
     author="Me",
     description="",
     packages=[],
